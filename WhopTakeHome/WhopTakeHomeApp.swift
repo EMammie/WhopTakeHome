2ba02982c0338fa4351028cc93d1769be2805cf7
Eugene Mammie <emammie@gmail.com>
2024-08-06 18:19:02 -0400

    Initial Commit

---
+++ WhopTakeHome/WhopTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhopTakeHomeApp.swift
+//  WhopTakeHome
+//
+//  Created by  Eugene Mammie on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WhopTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
